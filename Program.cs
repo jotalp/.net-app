8038f50413663209dd839addc6d39e8444cc2d3c
J P <jotalpim@gmail.com>
2021-09-25 07:38:12 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! mad mad World");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
